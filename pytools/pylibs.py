872da33d049acf7fe77a8d01ccf5ef816fe5b442
EnderYT6222 <yigitsirvanyt@gmail.com>
2025-08-06 21:39:42 +0300

    Create pylibs.py

---
+++ pytools/pylibs.py
@@ -0,0 +1,13 @@
+# pylibs.py
+# Orbit diline Python kütüphaneleri sağlayan köprü dosyası
+
+import os
+import sys
+import random
+# Buraya başka modüller de eklenebilir
+
+PYLIBS = {
+    "os": os,
+    "sys": sys,
+    "random": random,
+}
